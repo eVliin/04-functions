1473d35511efe963b0f6d00a0591c4251d80b782
eVliin <evelynma.b7@gmail.com>
2022-06-19 18:55:00 -0300

    end

---
+++ script.js
@@ -0,0 +1,37 @@
+
+Name = "Evelyn"
+age = 17
+musicType = "Any"
+
+function log() {
+    console.log("log :)")
+}
+
+function nome(Name) {
+    console.log(Name)
+}
+
+function info(Name, age, musicType) {
+    console.log(Name)
+    console.log(age)
+    console.log(musicType)
+}
+
+function media(film, music) {
+    console.log(film)
+    console.log(music)
+}
+
+function triple(n) {
+    return n * 3
+}
+
+log()
+
+nome(Name)
+
+info(Name, age, musicType)
+
+media("morbius", "say so")
+
+console.log(triple(3))
